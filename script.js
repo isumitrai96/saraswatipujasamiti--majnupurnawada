99eac5fbfb28a5014d1dc36b32a9e630cb5f9b8e
isumitrai96 <skroy131102@gmail.com>
2025-07-13 00:53:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const nameInput = document.getElementById("name");
+const amountInput = document.getElementById("amount");
+const upiLink = document.getElementById("upiLink");
+
+amountInput.addEventListener("input", () => {
+  const name = nameInput.value.trim() || "Sumit Roy";
+  const amount = amountInput.value;
+
+  if (amount > 0) {
+    const upiURL = `upi://pay?pa=skroy131102@ybl&pn=${encodeURIComponent(name)}&am=${amount}&cu=INR`;
+    upiLink.href = upiURL;
+    upiLink.style.display = "inline-block";
+  } else {
+    upiLink.style.display = "none";
+  }
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("toggleBtn");
+  const sidebar = document.getElementById("sidebar");
+
+  toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("show");
+  });
+});
+
